5815eef55651392b63e5cf69ac7f605a7fe37e5e
Marcelo Silva <marcelos@microsoft.com>
2019-07-03 17:03:03 -0500

    demo changes

--- textapi.ps1
+++ textapi.ps1
@@ -1,3 +1,116 @@
+function ExtractManufacturer($array)
+{
+    $list = @("LENNOX","YORK","TRANE","ENGA","SDMO","ONON","CLEAVER","NRTL","MARATHON","GSW","MITSUBISHI","CARRIER","MCQUAY","WALCHEM","KOHLER","SIEMENS")
+    foreach ($item in $list) 
+    {
+        if (($array.text -match $item).length -gt 0)
+        {
+            return $item
+        }            
+    }
+    #take a guess using the first recognized text item
+    return $array[0].text
+}
+
+function ExtractAnyManufacturerData ($array, $regEx, $cleanupRegexArray)
+{
+    $found = $false
+    for ($i = 0; ($i -lt $array.Count) -and !($found); $i++) 
+    {
+        if ($array[$i].text -match $regEx -and ! $found)       
+        {
+            $found = $true
+            $text = ($array[$i].text -replace $regEx,"").Trim()
+            foreach ($regExItem in $cleanupRegexArray) 
+            {
+                $text = ($text -replace $regExItem,"").Trim()
+            }
+
+            #remove original ocurrence
+            if ($text.length -gt 6)
+            {
+                return $text
+            }
+            else 
+            {
+                if ($array[$i+1].text.length -gt 8)
+                {
+                    return $array[$i+1].text
+                }
+                else 
+                {
+                    return $array[$i-1].text
+                }
+            }           
+            $found = $true
+        } 
+    }  
+}
+
+function ExtractData ($jsonObject)
+{
+    #this function expects the json object to follow the OCR schema
+
+    #loop thru the collection of boxes and words to assign distance from top left corner
+    $array = @()
+
+    foreach ($item in $jsonObject.recognitionResult.lines) 
+    {
+       $x = $item.boundingBox[0]
+       $y = $item.boundingBox[1]
+
+       $array += New-Object PSCustomObject -Property @{
+           x=$x;
+           y=$y;
+           boundingBox=$item.boundingBox;
+           text=$item.text.Trim()
+       }
+    }
+
+    #sort by vertical distance then left-right
+    $array = $array | Sort-Object y,x
+    
+    #usually the first text detected is the manufacturer
+    $manufacturer = ExtractManufacturer $array
+
+    # switch ($manufacturer)
+    # {
+    #     "LENNOX" {$data = ExtractLennoxData $array; break}
+    #     "TRANE"  {$data = ExtractTraneData $array; break}
+    #     "YORK"   {$data = ExtractYorkData $array; break}
+    #     Default  {$data = ExtractOtherData $array; break}
+    # }
+
+    switch ($manufacturer)
+    {
+        "LENNOX" {
+                    $modelNumber = ExtractAnyManufacturerData $array "M[I\/]?N" @("\:")  
+                    $serialNumber = ExtractAnyManufacturerData $array "S[I\/]?N" @("\:") 
+                    break
+                 }
+        "TRANE"  {
+                    $modelNumber = ExtractAnyManufacturerData $array "MOD\.|MODEL" @("VOLTS","[0-9]{3}\/[0-9]{3}")  
+                    $serialNumber = ExtractAnyManufacturerData $array "SERIAL" @() 
+                    break
+                 }
+        "YORK"   {
+                    $modelNumber = ExtractAnyManufacturerData $array "UNIT MODEL" @()  
+                    $serialNumber = ExtractAnyManufacturerData $array "SERIAL NO\." @() 
+                    break
+                 }
+
+        Default  {  $modelNumber = ExtractAnyManufacturerData $array "MODEL" @()  
+                    $serialNumber = ExtractAnyManufacturerData $array "SERIAL" @() 
+                    break
+                 }
+    }
+
+    New-Object -TypeName PSCustomObject -Property @{
+        manufacturer = $manufacturer;
+        modelNumber = $modelNumber;
+        serialNumber = $serialNumber;
+    }
+}
 
 Function RecognizeText($baseUrl, $imgUrl)
 {
@@ -19,7 +132,8 @@ function GetRecognizeTextOperationResult ($operationLocation)
 {
     do 
     {
-        Start-Sleep -Seconds 5
+        Write-host "Waiting for API..."
+        Start-Sleep -Seconds 3
         $headers = @{   "Ocp-Apim-Subscription-Key"="$subscriptionKey"                        
         }
         $responseRaw = (Invoke-WebRequest -Uri $operationLocation -Headers $headers -Method Get).Content
@@ -57,9 +171,9 @@ $storageContext = New-AzStorageContext -StorageAccountName $storageAccountName -
 #$summaryFile = "C:\work\cbre\textapi_summary.txt"
 #"" > $summaryFile
 
-$resultsPath = "D:\Work\CBRE\AI\testec-results\trane"
+#$resultsPath = "D:\Work\CBRE\AI\test-recog-results\lennox"
 
- foreach ($blob in Get-AzStorageBlob -Context $storageContext -Container "imgs" -Prefix "nameplate-trane") 
+ foreach ($blob in Get-AzStorageBlob -Context $storageContext -Container "imgs" -Prefix "nameplate-lennox") 
  {    
      $blobFullUrl = $blobContainerUrl + $blob.Name + $sasQueryString     
      $blobName = $blob.Name.Replace("/","-")
@@ -69,9 +183,14 @@ $resultsPath = "D:\Work\CBRE\AI\testec-results\trane"
      $operationLocation = RecognizeText $baseUrl $blobFullUrl
 
      $response = GetRecognizeTextOperationResult $operationLocation          
-     
-     $response >> "$resultsPath\$($blobName).json"
-     $response = $response | ConvertFrom-Json
+     #$response
+     $response = $response | ConvertFrom-Json    
+
+     $data = ExtractData $response
+     Write-output "File : $($blobName) - Data extracted: Manufacturer = $($data.manufacturer); Model = $($data.modelNumber); Serial = $($data.serialNumber)" 
+
+     #$response >> "$resultsPath\$($blobName).json"
+     #$response = $response | ConvertFrom-Json
 
      Write-Output "Analysis complete for $blobName"
 #     #extract manufacturer, serial number and model number
